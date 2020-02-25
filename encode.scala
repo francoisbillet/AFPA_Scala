d4951a2020c3dadd08dffa246d6d981932d3fc99
31010-16-07 <31010-16-07@exchange.ad.afpanet>
2020-02-25 10:23:12 +0100

    Retrait des try catch et ajout des modulos pour le décalage de l'encodage

--- encode.scala
+++ encode.scala
@@ -6,34 +6,48 @@ object encode {
             
         // println('a'.toInt) // 97
 
-        try {
+        if (args.length > 1) {
             var wordToEncode : String = args(0);
             var step : String = args(1);
             println(wordToEncode.map(c => changeCharacter(c,step.toInt)));
         }
-        catch {
-            case nb : NumberFormatException => handleNbException("Le type des paramètres n'est pas bon")
-            case param : ArrayIndexOutOfBoundsException => handleParamException("Il faut entrer au minimum 2 arguments")
-            case other : Throwable => println(other)
+        else {
+            println("Il faut entrer au minimum 2 arguments")
         }
-        finally {
-            // println("This code is always executed !")
-        }
+
+        // println("acv".contains('c'))
+        // println("acv".indexOf('c'))
+
+        // ON NE FAIT DE TRY CATCH QUE QUAND ON NE SAIT PAS CE QUI PEUT ARRIVER
+
+        // try {
+        //     var wordToEncode : String = args(0);
+        //     var step : String = args(1);
+        //     println(wordToEncode.map(c => changeCharacter(c,step.toInt)));
+        // }
+        // catch {
+        //     case e : NumberFormatException => handleNbException("Le type des paramètres n'est pas bon :\n" + e)
+        //     case e : ArrayIndexOutOfBoundsException => handleParamException("Il faut entrer au minimum 2 arguments :\n" + e)
+        //     case other : Throwable => println(other)
+        // }
+        // finally {
+        //     // println("This code is always executed !")
+        // }
             
     }
 
     def changeCharacter(c: Char, step: Int): Char = {
-        if (step < 0 || c.toInt + step > 127) {
-            throw new Exception("Décalage trop grand !")
+        if (step < 0) {
+            return ((c.toInt + step.abs) % 256).toChar
         }
-        return (c.toInt + step).toChar
+        return ((c.toInt + step) % 256).toChar
     }
 
-    def handleNbException(str: String): Unit = {
-        println(str)
-    }
+    // def handleNbException(str: String): Unit = {
+    //     println(str)
+    // }
 
-    def handleParamException(str: String): Unit = {
-        println(str)
-    }
+    // def handleParamException(str: String): Unit = {
+    //     println(str)
+    // }
 }
