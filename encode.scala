644bc8730acf6eb9bfc3a8aed81a4f76ea9e84b6
31010-16-07 <31010-16-07@exchange.ad.afpanet>
2020-02-25 11:25:00 +0100

    Suppression des commentaires

--- encode.scala
+++ encode.scala
@@ -3,8 +3,6 @@ import scala.util.matching.Regex
 object encode {
 
     def main(args: Array[String]): Unit = {
-            
-        // println('a'.toInt) // 97
 
         if (args.length > 1) {
             var wordToEncode : String = args(0);
@@ -14,25 +12,6 @@ object encode {
         else {
             println("Il faut entrer au minimum 2 arguments")
         }
-
-        // println("acv".contains('c'))
-        // println("acv".indexOf('c'))
-
-        // ON NE FAIT DE TRY CATCH QUE QUAND ON NE SAIT PAS CE QUI PEUT ARRIVER
-
-        // try {
-        //     var wordToEncode : String = args(0);
-        //     var step : String = args(1);
-        //     println(wordToEncode.map(c => changeCharacter(c,step.toInt)));
-        // }
-        // catch {
-        //     case e : NumberFormatException => handleNbException("Le type des paramètres n'est pas bon :\n" + e)
-        //     case e : ArrayIndexOutOfBoundsException => handleParamException("Il faut entrer au minimum 2 arguments :\n" + e)
-        //     case other : Throwable => println(other)
-        // }
-        // finally {
-        //     // println("This code is always executed !")
-        // }
             
     }
 
@@ -42,12 +21,4 @@ object encode {
         }
         return ((c.toInt + step) % 256).toChar
     }
-
-    // def handleNbException(str: String): Unit = {
-    //     println(str)
-    // }
-
-    // def handleParamException(str: String): Unit = {
-    //     println(str)
-    // }
 }
